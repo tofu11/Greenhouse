14377a05e297496662967243d717ac202b8bbce6
tofu11 <87494814+tofu11@users.noreply.github.com>
2023-10-11 11:02:03 -0400

    init

---
+++ greenhouse.ino
@@ -0,0 +1,31 @@
+//herb blushing apeldoorn
+//How moist the water levels are 
+int moist;
+
+
+void dontwater(){
+    void;
+}
+//TODO WATER THE SYSTEM BY ACTIVATING A BUTTON OR SOMETHING 
+void water(){
+    void;
+}
+//returns the level of water for sensor function
+int sensewater(){
+}
+
+void sensor(int ideal_moisture,int current_moisture){
+    //if the ideal moisture is less than the current moisture we activate water
+    if(ideal_moisture<current_moisture){
+        water();
+    }
+    if(ideal_moisture>current_moisture){
+        dontwater();
+    }
+
+
+    void;
+}
+
+//herb #2
+
