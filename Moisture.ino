12ec59cbe71ee4ffb9d1505489f8b56f2f73d165
tofu11 <87494814+tofu11@users.noreply.github.com>
2023-10-15 20:18:13 -0400

    stuff

--- Moisture.ino
+++ Moisture.ino
@@ -1,44 +1,49 @@
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
- 
+const int relayPin = 8;
+const int sensorPin = A0;
+float sensorValue = 0;
+
 void setup() {
-  Serial.begin(9600);
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
-  delay(1000);
-  lcd.setCursor(0, 0);
-  lcd.print("IRRIGATION");
-  lcd.setCursor(0, 1);
-  lcd.print("SYSTEM IS ON ");
-    lcd.print("");
-    delay(3000);
-  lcd.clear();
+Serial.begin(9600);
+pinMode(relayPin, OUTPUT)
+pinMode(sensorPin, INPUT);
+digitalWrite(relayPin, HIGH);
+delay(2000);
 }
- 
+
 void loop() {
-  int value = analogRead(A0);
-  Serial.println(value);
-  if (value > 950) {
-    digitalWrite(2, LOW);
-    lcd.setCursor(0, 0);
-    lcd.print("Water Pump is ON ");
-  } else {
-    digitalWrite(2, HIGH);
-    lcd.setCursor(0, 0);
-    lcd.print("Water Pump is OFF");
+Serial.print("MOISTURE LEVEL: ");
+sensorValue = analogRead(sensorPin);
+//sets "sensorValue" to input value from A0
+Serial.print(sensorValue);
+//prints moisture level
+if(sensorValue>750)
+{
+digitalWrite(relayPin, HIGH);
+//if moisture level is above 750, turns off pump
+}
+else
+{
+digitalWrite(relayPin, LOW);
+//if moisture level is not above 750, turns on pump
+}
+Serial.println();
+delay(1000);
+}
+Ziang Chen — Yesterday at 4:15 PM
+Image
+const int button= 2;
+const int led= 8;
+int buttonstate;
+void setup(){
+  pinMode(button,INPUT_PULLUP);
+  pinMode(led,OUTPUT);
+}
+void loop(){
+  buttonstate=digitalRead(button);
+  if (button==HIGH){
+    digitalWrite(led,LOW);
   }
- 
-  if (value < 300) {
-    lcd.setCursor(0, 1);
-    lcd.print("Moisture : HIGH");
-  } else if (value > 300 && value < 950) {
-    lcd.setCursor(0, 1);
-    lcd.print("Moisture : MID ");
-  } else if (value > 950) {
-    lcd.setCursor(0, 1);
-    lcd.print("Moisture : LOW ");
+  else{
+    digitalWrite(led,HIGH);
   }
 }
--- sprinkle.ino
+++ sprinkle.ino
@@ -1,37 +1,16 @@
-#define SENSOR_PIN 2
-#define BUZZER_PIN 3
-#define RELAY_PIN 4
-#define SPRINKLER_START_DELAY 5000  //5 seconds 
-#define SPRINKLER_ON_TIME 3000      //3 seconds Sprinkler on time
-
-unsigned long previousTime = millis();
-
-void setup() 
-{
-  pinMode(RELAY_PIN, OUTPUT);
-  pinMode(SENSOR_PIN, INPUT);  
+const int button= 2;
+const int led= 8;
+int buttonstate;
+void setup(){
+  pinMode(button,INPUT_PULLUP);
+  pinMode(led,OUTPUT);
 }
-
-void loop() 
-{
-  //If there is fire then the sensor value will be LOW else the value will be HIGH
-  int sensorValue = digitalRead(SENSOR_PIN);
-
-  //There is fire
-  if (sensorValue == LOW)
-  {
-    analogWrite(BUZZER_PIN, 50);                          //Turn on buzzer
-
-    if (millis() - previousTime > SPRINKLER_START_DELAY)  //We will wait for few seconds before sprinkler can be started once fire is detected.
-    {
-      digitalWrite(RELAY_PIN, LOW);                       //Relay is low level triggered relay so we need to write LOW to switch on the light 
-      delay(SPRINKLER_ON_TIME);                           //Keep sprinkler on for sometime.
-    }
+void loop(){
+  buttonstate=digitalRead(button);
+  if (button==HIGH){
+    digitalWrite(led,LOW);
   }
-  else
-  {
-    analogWrite(BUZZER_PIN, 0);    
-    digitalWrite(RELAY_PIN, HIGH); 
-    previousTime = millis();  
+  else{
+    digitalWrite(led,HIGH);
   }
-} 
+}
