07bff4c715a6d74c8d0528650f38bcb88fa2e4ff
Spencer Login <spencerlogin03@gmail.com>
2023-10-18 16:49:02 -0400

    Sprinke

--- sprinkle/sprinkle.ino
+++ sprinkle/sprinkle.ino
@@ -1,7 +1,10 @@
 const int SENSOR = A0;
 const int PUMP = 9;
-int BASE_LVL = 0;
-int MOISTURE_LVL = 600;
+int BASE_LVL = 600;
+int MOISTURE_LVL;
+
+//lower number = wet
+//higher number = dry
 
 void setup(){
   pinMode(SENSOR,INPUT);
@@ -13,12 +16,14 @@ void loop(){
   MOISTURE_LVL = analogRead(SENSOR);
   Serial.print(MOISTURE_LVL);
   Serial.print("\n");
-  if (MOISTURE_LVL > BASE_LVL){
+  if (MOISTURE_LVL < BASE_LVL){
     Serial.print("Pump should run here\n");
     digitalWrite(PUMP, HIGH);
     delay(2000);
   }
   else{
     digitalWrite(PUMP, LOW);
+    Serial.print("Pump should not run here\n");
+    delay(2000);
   }
 }
