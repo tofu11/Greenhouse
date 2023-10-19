ba4d0f909a47fdb98c16f2fa747669c31bf66aaf
Spencer Login <spencerlogin03@gmail.com>
2023-10-19 12:53:52 -0400

    Switched APIs for fetching weather data

--- WiFi/WiFi.ino
+++ WiFi/WiFi.ino
@@ -2,20 +2,19 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <Arduino_JSON.h>
-#include <string.h>
-#include <iostream>
-using namespace std;
 
 const char *ssid="UB_Connect";
-const char *URL="http://api.weatherapi.com/v1/current.json";
-const char *API_KEY="a3607837a6a044f388275829231910";
 unsigned long lastConnectionTime = 0;
-const unsigned long counter = 10L * 1000L; // delay between updates, in milliseconds
+//const unsigned long counter = 10L * 1000L; // delay between updates, in milliseconds
+//const String API_KEY = "a3607837a6a044f388275829231910";
+const String CITY = "Buffalo";
 
 String httpGETRequest(String serverName);
 
 WiFiClientSecure client;
-String server_name = "http://api.weatherapi.com/v1/current.json?key=a3607837a6a044f388275829231910&q=Buffalo&aqi=no";
+HTTPClient http;
+//String server_name = "api.weatherapi.com/v1/current.json";
+String server_name = "api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relativehumidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,cloudcover&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&forecast_days=1";
 String response;
 
 void setup() {
@@ -30,12 +29,13 @@ void setup() {
   Serial.println();
   Serial.println("Connecting ...");
 
-  while(WiFi.status() != WL_CONNECTED) {
+  unsigned long timeout = millis();
+  while(millis() - timeout < 10000 && WiFi.status() != WL_CONNECTED) {
     delay(500);
+    timeout += 500;
     Serial.println(".");
   }
   Serial.println("Connected!");
-  
   Serial.print("NodeMCU IP Address:");
   Serial.print(WiFi.localIP());
 
@@ -43,29 +43,7 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  if(WiFi.status() == WL_CONNECTED) {
-    response = httpGETRequest(server_name);
-    Serial.print("Raw response: ");
-    Serial.println(response);
-    JSONVar response_obj = JSON.parse(response);
-
-    if(JSON.typeof(response_obj) == "undefined") {
-      Serial.println("Parsing input failed");
-      return;
-    }
-    Serial.print("JSON Object: ");
-    Serial.println(response_obj);
-
-    JSONVar keys = response_obj.keys();
-
-    for(int i=0; i<keys.length(); i++) {
-      JSONVar value = response_obj[keys[i]];
-      Serial.print(keys[i]);
-      Serial.print(" = ");
-      Serial.print(value);
-    }  
-    lastConnectionTime = millis();
-    if (millis() - lastConnectionTime > counter) {  
+    if (millis() - lastConnectionTime > 5000) {  
       response = httpGETRequest(server_name);
       Serial.println(response);
       JSONVar response_obj = JSON.parse(response);
@@ -85,37 +63,32 @@ void loop() {
         Serial.print(" = ");
         Serial.print(value);
       }
-    }
-  }else {
-    Serial.println("WiFi Disconnected");
+      lastConnectionTime = millis();
   }
-  lastConnectionTime = millis();
 }
   
 String httpGETRequest(String serverName) {
-  WiFiClient client;
-  HTTPClient http;
-
-  // Your IP address with path or Domain name with URL path 
-  http.begin(client, server_name);
-
+  
+  //  String URL = serverName + "?key=" + API_KEY + "&q=" + CITY + "&aqi=no";
+  String URL = serverName;
+  http.begin(client, URL);
+  
   // If you need Node-RED/server authentication, insert user and password below
   //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
-
+  
   // Send HTTP POST request
   int httpResponseCode = http.GET();
-
+  
   String payload = "{}"; 
-  Serial.print("Response: ");
-  Serial.println(http.getString());
-  Serial.println();
   if(httpResponseCode > 0) {
+    Serial.print("Response: ");
+    Serial.println(http.getString());
+    Serial.println();
     payload = http.getString();
   }
   else {
     Serial.print("Error code: ");
     Serial.println(httpResponseCode);
-    return payload;
   }
   // Free resources
   http.end();
